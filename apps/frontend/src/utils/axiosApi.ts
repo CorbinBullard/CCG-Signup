4cae01182226e752897502f20f78b57ba6ce4827
Corbin Bullard <corbindbullard@gmail.com>
2025-07-01 21:31:07 -0400

    changed base url

--- apps/frontend/src/utils/axiosApi.ts
+++ apps/frontend/src/utils/axiosApi.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/", // NestJS backend URL
+  baseURL: process.env.VITE_API_URL, // NestJS backend URL
 });
 
 // Interceptor to automatically attach token in headers
-api.interceptors.request.use(config => {
-  const token = localStorage.getItem('token');
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
@@ -14,15 +14,14 @@ api.interceptors.request.use(config => {
 });
 
 api.interceptors.response.use(
-  response => response,
-  error => {
+  (response) => response,
+  (error) => {
     if (error.response && error.response.status === 401) {
-      localStorage.removeItem('token');
-      window.location.href = '/login';
+      localStorage.removeItem("token");
+      window.location.href = "/login";
     }
     return Promise.reject(error);
   }
 );
-
 
 export default api;
--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -3,9 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   server: {
     port: 3000,
     cors: {
@@ -13,7 +11,7 @@ export default defineConfig({
     },
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
         secure: false,
       },
