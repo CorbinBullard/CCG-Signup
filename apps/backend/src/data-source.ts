26acccd59cd9e6fa0baacaf8c040e885af39b2de
Corbin Bullard <corbindbullard@gmail.com>
2025-07-03 12:36:35 -0400

    enities

--- apps/backend/src/data-source.ts
+++ apps/backend/src/data-source.ts
@@ -6,11 +6,16 @@ import appConfig from './config';
 import { DataSource } from 'typeorm';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions';
+import { join } from 'path';
+
+console.log(join(__dirname, 'dist/src/**/*.entity.js'));
 
 const dbConfig: PostgresConnectionOptions | SqliteConnectionOptions =
   appConfig().database;
 
 export const AppDataSource = new DataSource({
   ...dbConfig,
-  entities: ['dist/**/*.entity.js'],
+  entities: ['dist/src/**/*.entity.js'],
 });
+
+console.log(AppDataSource);
